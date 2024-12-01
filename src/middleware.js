93eae0265425d896a2e1e522f658c41cd0f94694
Abdullah-Az-Zahur <abdullah.az.zahur@gmail.com>
2024-12-01 09:52:04 +0600

    add : middleware

---
+++ src/middleware.js
@@ -0,0 +1,14 @@
+import { NextResponse } from "next/server";
+
+const user = true;
+export const middleware = (request) => {
+  if (!user) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  return NextResponse.next();
+};
+
+export const config = {
+  matcher: ['/'],
+};
